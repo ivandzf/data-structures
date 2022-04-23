3fdc8587a0121ddb254b98f971e5a0963ef91f99
Divananda Zikry Fadilla <divananda@ruangguru.com>
2022-04-23 10:16:04 +0700

    update golang linked list

--- go/singly_linked_list.go
+++ go/singly_linked_list.go
@@ -28,5 +28,80 @@ func (ll *LinkedList[T]) InsertLast(element T) {
 		}
 		current.Next = &newNode
 	}
+
 	ll.Count++
+}
+
+func (ll *LinkedList[T]) InsertFirst(element T) {
+	newNode := Node[T]{element, nil}
+	if ll.Head == nil {
+		ll.Head = &newNode
+	} else {
+		newNode.Next = ll.Head
+		ll.Head = &newNode
+	}
+
+	ll.Count++
+}
+
+func (ll *LinkedList[T]) GetNthNode(index uintptr) *Node[T] {
+	if index >= ll.Count {
+		return nil
+	}
+
+	current := ll.Head
+	for i := uintptr(0); i < index; i++ {
+		current = current.Next
+	}
+
+	return current
+}
+
+func (ll *LinkedList[T]) RemoveNthNode(index uintptr) {
+	if index >= ll.Count {
+		return
+	}
+	if index == 0 {
+		ll.Head = ll.Head.Next
+	} else {
+		current := ll.Head
+		for i := uintptr(0); i < index-1; i++ {
+			current = current.Next
+		}
+		current.Next = current.Next.Next
+	}
+
+	ll.Count--
+}
+
+func (ll *LinkedList[T]) RemoveFirst() {
+	if ll.Head == nil {
+		return
+	}
+
+	ll.Head = ll.Head.Next
+	ll.Count--
+}
+
+func (ll *LinkedList[T]) RemoveLast() {
+	if ll.Head == nil {
+		return
+	}
+
+	current := ll.Head
+	for current.Next != nil {
+		current = current.Next
+	}
+
+	current.Next = nil
+	ll.Count--
+}
+
+func (ll *LinkedList[T]) Print() {
+	current := ll.Head
+
+	for current != nil {
+		println(current.Element)
+		current = current.Next
+	}
 }
--- go/singly_linked_list_test.go
+++ go/singly_linked_list_test.go
@@ -8,6 +8,52 @@ import (
 
 func TestNewLinkedList(t *testing.T) {
 	ll := NewLinkedList[int]()
-	assert.Equal(t, nil, ll.Head)
 	assert.Equal(t, uintptr(0), ll.Count)
+	assert.Equal(t, nil, ll.Head)
+}
+
+func TestInsertLinkedList(t *testing.T) {
+	ll := NewLinkedList[int]()
+
+	// insert last
+	ll.InsertLast(1)
+	assert.Equal(t, uintptr(1), ll.Count)
+	assert.Equal(t, 1, ll.GetNthNode(0).Element)
+
+	// insert first
+	ll.InsertFirst(10)
+	assert.Equal(t, uintptr(2), ll.Count)
+	assert.Equal(t, 10, ll.GetNthNode(0).Element)
+}
+
+func TestRemoveLinkedList(t *testing.T) {
+	ll := NewLinkedList[int]()
+
+	ll.InsertLast(1)
+	ll.InsertLast(10)
+	ll.InsertLast(100)
+	assert.Equal(t, uintptr(3), ll.Count)
+
+	// remove last
+	ll.RemoveLast()
+	assert.Equal(t, uintptr(2), ll.Count)
+	assert.Equal(t, nil, ll.GetNthNode(2))
+
+	// remove first
+	ll.RemoveFirst()
+	assert.Equal(t, uintptr(1), ll.Count)
+	assert.Equal(t, 10, ll.GetNthNode(0).Element)
+}
+
+func TestPrint(t *testing.T) {
+	ll := NewLinkedList[int]()
+
+	ll.InsertLast(1)
+	ll.InsertLast(10)
+	ll.InsertLast(100)
+	ll.InsertLast(1000)
+	ll.InsertLast(10000)
+
+	ll.Print()
+	t.Log("1\n10\n100\n1000\n10000")
 }
